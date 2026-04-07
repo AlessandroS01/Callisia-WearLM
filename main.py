1706f911af5535674e65fc88bcbd2f266aede3b1
AlessandroS01 <it_seghini@outlook.com>
2026-04-07 15:51:04 +0200

    generated an initial schema for processing the dalia dataset. get_standardized_windows() created but must be refined properly using that schema

--- main.py
+++ main.py
@@ -1,52 +1,9 @@
-from utils.dalia.ecg.ecg_quality_measure import ECGQualityMeasure
-from utils.dalia.feature_extractor import FeatureExtractor
-from utils.dalia.dataset_handler import PPGDaliaDatasetHandler
-from utils.plot_handler import PlotHandler
-
-import pandas as pd
-
+from processing.dalia_processor import DaliaProcessor
 
 def main():
-    """
-    calculate_rr_intervals()"""
-    for i in range(1, 16):
-        patient = f"S{i}"
-        path_ecg = f"datasets/dalia/converted/{patient}/chest/chest_ECG.csv"
-        r_peaks_path = f"datasets/dalia/converted/{patient}/rpeaks.csv"
-        parquet_file = f"datasets/dalia/converted/{patient}/features/signal_quality.parquet"
+    processor = DaliaProcessor("datasets/dalia/converted/S1")
+    print(processor.get_standardized_windows())
 
-        extractor = FeatureExtractor(n_seconds=10, ecg_signal_path=path_ecg, r_peaks_path=r_peaks_path)
-
-        #extractor.signal_quality_index_retrieval(parquet_file)
-        print(extractor.calculate_peaks_f1_score())
-
-    #see_plot(path)
-
-def compute_quality(ecg_path, r_peaks_path):
-    measure = ECGQualityMeasure(10, ecg_signal_path=ecg_path, r_peaks_path=r_peaks_path)
-    print(measure.calculate_peak_f1())
-    #print(measure.signal_quality_index_retrieval("datasets/dalia/converted/S1/features/signal_quality.parquet"))
-
-
-def open_pickle_dataset(path, patient):
-    handler = PPGDaliaDatasetHandler(path)
-    handler.extract_data(f'datasets/dalia/converted/{patient}')
-    json_path = "datasets/dalia/converted/S1.json"
-    handler.convert_pkl_json(json_path)
-
-def see_plot(dataset):
-    plot_handler = PlotHandler(dataset)
-    plot_handler.create_ecg_plot()
-
-def calculate_rr_intervals():
-    extractor = FeatureExtractor()
-
-    for i in range(1, 16):
-        patient = f"S{i}"
-        intervals = extractor.calculate_rr_intervals(pd.read_csv(f"datasets/dalia/converted/{patient}/rpeaks.csv"), 700)
-        output_dir = f"datasets/dalia/converted/{patient}/features"
-        extractor.save_csv("rr_intervals", output_dir, intervals)
-        print(f"Patient: {patient} done")
 
 if __name__ == '__main__':
     main()
--- processing/dalia_processor.py
+++ processing/dalia_processor.py
@@ -2,11 +2,12 @@ import os
 
 import pandas as pd
 import numpy as np
+from pandas import DataFrame
 
 from utils.dalia.configuration import BVP_SAMPLING_RATE, ACC_SAMPLING_RATE, WINDOW_SIZE_SEC
 
 
-class DaliaProcessor():
+class DaliaProcessor:
     """
     Orchestrates the preprocessing of the PPG-DaLiA dataset for 1D-CNN ingestion.
 
@@ -17,9 +18,11 @@ class DaliaProcessor():
 
     Attributes:
         subject_dir (str): Path to the base directory of a single DaLiA subject.
-        window_size_sec (int): The duration of each data window in seconds (Default: 10).
-        bvp_hz (int): Target sampling rate for Blood Volume Pulse (Default: 64).
-        acc_hz (int): Original sampling rate for Accelerometer (Default: 32).
+        window_size_bvp (int): The size of the window for BVP data in samples.
+        window_size_acc (int): The size of the window for ACC data in samples.
+        bvp_data (pd.DataFrame): Raw BVP data. Shape: (N, 1)
+        acc_data (pd.DataFrame): Raw ACC data. Shape: (M, 3)
+        sqi_data (pd.DataFrame): Raw SQI data. Shape: (M, 1)
         """
 
     def __init__(self, subject_dir: str):
@@ -28,12 +31,13 @@ class DaliaProcessor():
         """
         super(DaliaProcessor, self).__init__()
         self.subject_dir = subject_dir
-        self.window_size_sec = WINDOW_SIZE_SEC
-        self.bvp_hz = BVP_SAMPLING_RATE
-        self.acc_hz = ACC_SAMPLING_RATE
 
-        self.bvp_data = self._retrieve_data("wrist/wrist_BVP.csv")
-        self.acc_data = self._retrieve_data("wrist/wrist_ACC.csv")
+        self.window_size_bvp = WINDOW_SIZE_SEC * BVP_SAMPLING_RATE
+        self.window_size_acc = WINDOW_SIZE_SEC * ACC_SAMPLING_RATE
+
+        self.bvp_data = self._retrieve_data("wrist/wrist_BVP.csv", "csv")
+        self.acc_data = self._retrieve_data("wrist/wrist_ACC.csv", "csv")
+        self.sqi_data = self._retrieve_data("features/signal_quality.parquet", "parquet")
 
     def _align_and_resample(self, bvp_df: pd.DataFrame, acc_df: pd.DataFrame) -> pd.DataFrame:
         """
@@ -71,7 +75,7 @@ class DaliaProcessor():
         Extracts sliding windows of fused sensor data and their corresponding labels.
 
         Iterates through the subject's timeline, extracting data blocks defined by
-        `window_size_sec` and `step_size_sec`. It discards any windows that fail
+        `window_size_sec`. It discards any windows that fail
         the `_apply_quality_mask` check.
 
         Returns:
@@ -82,11 +86,39 @@ class DaliaProcessor():
                   Shape: (num_valid_windows, sequence_length, num_channels)
 
                   Num channels must be 4 and represents [BVP, ACCx, ACCy, ACCz].
-                - y (np.ndarray): The 1D label array (e.g., instantaneous HR).
+                - y (np.ndarray): The 1D label array for the signal quality index (SQI).
                   Shape: (num_valid_windows)
         """
-        pass
+        # x filling
+        x_array_bvp = self._fill_arrays(feature="bvp")
+        print(np.array(x_array_bvp).shape)
+        x_array_acc = self._fill_arrays(feature="acc")
+        print(np.array(x_array_acc).shape)
+        x_array = []
 
-    def _retrieve_data(self, typology:str) -> pd.DataFrame:
+        # y filling
+        y_array = self._fill_arrays(feature="sqi")
+        print(np.array(y_array).shape)
+
+        return np.array(x_array), np.array(y_array)
+
+    def _fill_arrays(self, feature) -> list:
+
+        data = self.bvp_data if feature == "bvp" else self.acc_data if feature == "sqi" else self.sqi_data
+        window_size = self.window_size_bvp if feature == "bvp" else self.window_size_acc
+        step_size = window_size // 3
+
+        array = []
+
+        for step in range(0, len(data) - window_size + 1, step_size):
+            array.append(data.iloc[step:step + window_size])
+
+        return array
+
+    def _retrieve_data(self, typology:str, data_type: str) -> DataFrame:
         path = os.path.join(self.subject_dir, typology)
-        return pd.read_csv(path)
+        if data_type == "csv":
+            return pd.read_csv(path)
+        elif data_type == "parquet":
+            return pd.read_parquet(path)
+        return pd.DataFrame()
